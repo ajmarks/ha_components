e06b5bc6d59d1969b628c3a5ec9025e040daa42f
Jack Simbach <jack.simbach@gmail.com>
2023-08-02 19:27:34 -0400

    - missed a commit

--- custom_components/ge_home/entities/dehumidifier/dehumidifier_fan_speed_sensor.py
+++ custom_components/ge_home/entities/dehumidifier/dehumidifier_fan_speed_sensor.py
@@ -1,8 +1,40 @@
-from ..common import GeErdBinarySensor
+from ...devices import ApplianceApi
+from ..common import GeErdSensor
+from .dehumidifier_fan_options import DehumidifierFanSettingOptionsConverter
+from gehomesdk import ErdCodeType, ErdCodeClass, ErdDataType, ErdAcFanSetting
 
-class GeCcmPotNotPresentBinarySensor(GeErdBinarySensor):
+class GeDehumidifierFanSpeedSensor(GeErdSensor):
+    def __init__(
+        self, 
+        api: ApplianceApi, 
+        erd_code: ErdCodeType, 
+        erd_override: str = None, 
+        icon_override: str = None, 
+        device_class_override: str = None,
+        state_class_override: str = None,
+        uom_override: str = None,
+        data_type_override: ErdDataType = None
+    ):
+    
+        super().__init__(
+            api, 
+            erd_code, 
+            erd_override, 
+            icon_override, 
+            device_class_override,
+            state_class_override,
+            uom_override,
+            data_type_override
+        )
+
+        self._converter = DehumidifierFanSettingOptionsConverter()
+
     @property
-    def is_on(self) -> bool:
-        """Return True if entity is not pot present."""
-        return not self._boolify(self.appliance.get_erd_value(self.erd_code))
+    def native_value(self):
+        try:
+            value: ErdAcFanSetting = self.appliance.get_erd_value(self.erd_code)
+            return self._converter.to_option_string(value)
+        except KeyError:
+            return None
+
     
